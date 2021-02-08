f2c4cbdb72a81937550cf760f2462988300648b7
Polad <samadzada.polad@gmail.com>
2021-02-08 21:17:51 +0100

    A class to provide API URIs

---
+++ DAP_Mobile/DAP_Mobile/API/ApiUri.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAP_Mobile.API
+{
+    public static class ApiUri
+    {
+        private static string baseUri;
+        private static string appointments;
+        private static string treatments;
+        private static string patients;
+        private static string products;
+        private static string payments;
+
+        static ApiUri()
+        {
+            baseUri = "https://dapapi.herokuapp.com/api/";
+            appointments = $"{baseUri}appointments";
+            treatments = $"{baseUri}treatments";
+            patients = $"{baseUri}patients";
+            products = $"{baseUri}products";
+            payments = $"{baseUri}payments";
+        }
+
+        public static string Appointments(Dictionary<string, string> conditions)
+        {
+            if(conditions.Keys.Count == 0)
+            {
+                return appointments;
+            }
+
+            string uri = $"{appointments}?{GetQuery(conditions)}";
+
+            return uri;
+        }
+
+        public static string Treatments(Dictionary<string, string> conditions)
+        {
+            if (conditions.Keys.Count == 0)
+            {
+                return treatments;
+            }
+
+            string uri = $"{treatments}?{GetQuery(conditions)}";
+
+            return uri;
+        }
+
+        public static string Patients(Dictionary<string, string> conditions)
+        {
+            if (conditions.Keys.Count == 0)
+            {
+                return patients;
+            }
+
+            string uri = $"{patients}?{GetQuery(conditions)}";
+
+            return uri;
+        }
+
+        public static string Payments(Dictionary<string, string> conditions)
+        {
+            if (conditions.Keys.Count == 0)
+            {
+                return payments;
+            }
+
+            string uri = $"{payments}?{GetQuery(conditions)}";
+
+            return uri;
+        }
+
+        public static string Products(Dictionary<string, string> conditions)
+        {
+            if (conditions.Keys.Count == 0)
+            {
+                return products;
+            }
+
+            string uri = $"{products}?{GetQuery(conditions)}";
+
+            return uri;
+        }
+
+        private static string GetQuery(Dictionary<string, string> conditions)
+        {
+            string query = "";
+
+            foreach(KeyValuePair<string, string> pair in conditions)
+            {
+                query += $"{pair.Key}={pair.Value}&";
+            }
+
+            query.TrimEnd('&');
+
+            return query;
+        }
+    }
+}
