f2cb1cecaf7d340ebb2f12f5c5ee9e4b270b9a16
Polad <samadzada.polad@gmail.com>
2021-02-14 17:40:27 +0100

    pull to refresh the timeline

--- DAP_Mobile/DAP_Mobile/ViewModels/DailyTimelineViewModel.cs
+++ DAP_Mobile/DAP_Mobile/ViewModels/DailyTimelineViewModel.cs
@@ -3,14 +3,35 @@ using DAP_Mobile.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace DAP_Mobile.ViewModels
 {
-    public class DailyTimelineViewModel
+    public class DailyTimelineViewModel : INotifyPropertyChanged
     {
+        private bool isRefreshing;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsRefreshing
+        {
+            get
+            {
+                return isRefreshing;
+            }
+            set
+            {
+                isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+        public ICommand RefreshCommand { get; }
         public ObservableCollection<Appointment> Appointments { get; set; }
         public DailyTimelineViewModel()
         {
+            RefreshCommand = new Command(RefreshHandler);
             Appointments = new ObservableCollection<Appointment>();
             Populate(DateTime.Now);
         }
@@ -25,6 +46,21 @@ namespace DAP_Mobile.ViewModels
             foreach(Appointment appointment in appointments)
             {
                 Appointments.Add(appointment);
+            }
+        }
+
+        private void RefreshHandler()
+        {
+            Appointments.Clear();
+            Populate(DateTime.Now);
+            IsRefreshing = false;
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
             }
         }
     }
